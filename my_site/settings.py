75f60c40f335406c479e38be2253726dfd6a6963
Aurélien Beveraggi <65907063+Aurelien-Be@users.noreply.github.com>
2022-07-02 11:31:08 +0300

    bbcode

--- my_site/settings.py
+++ my_site/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'precise_bbcode'
 ]
 
 ROOT_URLCONF = 'my_site.urls'
