4ee1e711e5f961c38a4afd26fbc012a99931622b
Aurelien-Be <abeveraggi@netcourrier.com>
2022-08-01 17:36:58 +0300

    internalization

--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,18 @@
 from django import forms 
-from .models import Comments 
+from .models import Comments
+from django.utils.translation import gettext_lazy
+
 
 class CommentForm(forms.ModelForm):
     class Meta: #meta class to configure it
         model = Comments
         exclude = ['post']
         labels = {
-        "user_name" : "Your Name",
-        "email" : "Your Email",
-        "body": "Your Comment"
+        "user_name" : (gettext_lazy("Your Name")),
+        "email" : (gettext_lazy("Your Email")),
+        "body": (gettext_lazy("Your Comment"))
         }
         widgets = {            
-        'body': forms.Textarea(
-         attrs={'placeholder': 'Enter your comment here (you can use bbcode)'}),
+            'body': forms.Textarea(
+                attrs={'placeholder':(gettext_lazy('Enter your comment here (you can use bbcode)'))}),
         }
