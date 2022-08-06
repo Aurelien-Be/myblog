3106774ebfacddce361081dabdb309a2aa655402
Aurelien-Be <abeveraggi@netcourrier.com>
2022-08-06 19:58:33 +0300

    simplification url.py

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,24 +1,10 @@
-"""my_site URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
-from django.urls import path, include, re_path
+from django.contrib import admin
+from django.urls import path, include
 #with include imported, we can register another path coming from our app blog 
-from django.conf.urls.static import static
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,8 +18,9 @@ urlpatterns += i18n_patterns (
 #to edit the translations in an admin-like module
 if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += [
-        re_path(r'^rosetta/', include('rosetta.urls'))
+        path('rosetta/', include('rosetta.urls'))
     ]
+
 
 
 
