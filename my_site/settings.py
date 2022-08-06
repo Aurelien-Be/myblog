6911f5653ce49c14b62cd2c8527ca13cc67a1189
Aurelien-Be <abeveraggi@netcourrier.com>
2022-08-06 22:47:37 +0300

    rosetta

--- my_site/settings.py
+++ my_site/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'precise_bbcode'
+    'precise_bbcode',
+    'rosetta'
 ]
 
 MIDDLEWARE = [
