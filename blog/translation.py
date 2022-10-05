389f825deabfb46243074f11ba2d1ccea495487f
Aurelien-Be <abeveraggi@netcourrier.com>
2022-10-05 17:32:33 +0300

    translation.py

---
+++ blog/translation.py
@@ -0,0 +1,12 @@
+from modeltranslation.translator import translator, register, TranslationOptions
+from .models import Tag, Post
+
+class TagTranslationOptions(TranslationOptions):
+   fields = 'caption'
+
+class PostTranslationOptions(TranslationOptions):
+    fields=("title", 'excerpt','slug', 'content', 'tags')
+
+translator.register(Post, PostTranslationOptions)
+translator.register(Tag, TagTranslationOptions)
+
